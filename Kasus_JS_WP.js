c266f95430fa9ceb5b2e4bef124727db2d626a6c
Adyfas <adymycode@gmail.com>
2024-05-19 11:33:03 +0700

    Tugasss

---
+++ Kasus_JS_WP.js
@@ -0,0 +1,104 @@
+
+//kasus ini mungkin mudah jadi selamat mengerjakan
+
+
+// kasus pertama gaesss yaitu function
+
+function Hitung(angkaP, angkaD){
+    console.log('Gimana caranya agar nilai dari parameter angkaP dan angka D itu di jumlahkan ??')
+    const hasil = 'Gimana?'
+    console.log("hasil dari "+ angkaP +' di jumlahkan dengan ' + angkaD+ " Hasil nya: " +hasil)
+    return 
+}
+
+Hitung(1, 5)
+
+
+
+
+
+
+
+
+//kasus kedua gaesss yaitu array Plus function
+const arraySaya = [1,2,3,4,5,6]
+
+Hitung(/* nilai array index ke 0*/ /* nilai array index ke 3 */)
+console.log('Gimana hayooo')
+
+/* jadi di isi dengan salah satu nilai array yang ada di arraySaya misalnya
+ kamu mau ambil nilai array ke 0 dan gimana caranya ambil nya gitu */
+//gimana caranya function Hitung itu di isi salah satu nilai array yang ada di const arraySaya???
+
+
+
+
+
+
+
+/*
+Kasus selanjutnya looping nihhh brayyy gampang kok bikin setengah segitiga!!
+
+buatlah output nya agar seperti ini:
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+
+menggunakan loop for 
+*/
+for (let i = 1; i <= 5; i++) {
+
+    //coding
+
+    console.log(output);
+}
+
+
+
+
+
+
+
+//Kasus Terakhir nihh semmoga berhasil 
+
+
+let data = {
+    nama:'samsul',
+    umur:23,
+    energi:10,
+    salam: function(){
+        console.log('Ayoo Gimana caranya memanggil fungsi ini?? ouh ya lupa salam kepada '+ this.nama)
+    },
+    makan: function( porsi ){
+        const makan = this.energi + porsi
+        console.log('Ayooo Gimana caranya menambahkan energi si '+ this.nama+ ' dengan memanggil const makan')
+        console.log(this.nama+ ' dia memakan sebanyak '+ porsi + ' dan energi di bertambah sebanyak '+ porsi)
+        console.log('energi si '+this.nama+' adalah'+ this.energi)
+    }
+}
+
+//ayooo bisa lah gampang kok ini dasar ada di pembelajaran tetang javascript yang ada di repositori ini lohhh
+
+console.log(data) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// dan yaaa sekian saja tugasnya silakan kerjakan di file ini, dan misalkan kalian udah selesai hebat!!!🤗 
+// ketika sudah selesai tugasnya bisa lahh kirim ke repositori github kalian masing masing lalu kirim link repo. nya ke admin gitu nanti kita review bareng bareng ok, Keep Koding!! Lanjut Apa lagi nihhhh!!
+
